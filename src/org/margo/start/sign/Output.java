9499769f81e2a4a252f362d0f6f63853261f4bdc
MargoSamo <margo_ii@mail.ru>
2022-07-01 12:53:23 +0500

    beginning of the game

--- src/org/margo/start/sign/Output.java
+++ src/org/margo/start/sign/Output.java
@@ -2,13 +2,9 @@ package org.margo.start.sign;
 
 public class Output {
     public static void main(String[] args) {
-        Output o = new Output();
-        o.go();
-    }
 
-    void go() {
         int y = 7;
-        for(int x = 1; x < 8; x++) {
+        for (int x = 1; x < 8; x++) {
             y++;
             if (x > 4) {
                 System.out.print(++y + " ");
