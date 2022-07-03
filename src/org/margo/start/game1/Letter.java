ab427be5a575693182c506dbbc8fd7cabf0c3030
MargoSamo <margo_ii@mail.ru>
2022-07-03 21:48:16 +0500

    new games Letter

--- src/org/margo/start/game1/Letter.java
+++ src/org/margo/start/game1/Letter.java
@@ -1,2 +1,22 @@
-package org.margo.start.game1;public class Letter {
+package org.margo.start.game1;
+
+import java.io.IOException;
+
+public class Letter {
+    public static void main(String[] args) throws IOException {
+        char ch, answer = 'b';
+
+        System.out.println("Какую букву я загадала ?");
+        System.out.print("Попытайтесь её угадать: ");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer) {
+            System.out.print("Поздравляю! ");
+        } else if (ch > answer) {
+            System.out.print("Перестарался! ");
+        } else {
+            System.out.print("Недостарался! ");
+        }
+    }
 }
