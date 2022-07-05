ff36c8f35d08015efccc58ea8da370676cfae45a
MargoSamo <margo_ii@mail.ru>
2022-07-05 22:03:00 +0500

    new fail

--- src/org/margo/start/scanner/Solution.java
+++ src/org/margo/start/scanner/Solution.java
@@ -1,5 +1,6 @@
 package org.margo.start.scanner;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Solution {
@@ -11,5 +12,14 @@ public class Solution {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
 
+        String str = "10 80 40 60";
+        Scanner scanner = new Scanner(str);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a > 5) {
+        System.out.println(a + b);
+        } else {
+        System.out.println(a - b);
+        }
     }
 }
