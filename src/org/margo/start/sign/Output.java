1688ad9a914b3f9657cc925aeea0a92b23007c4f
MargoSamo <margo_ii@mail.ru>
2022-07-08 14:49:32 +0500

    equals

--- src/org/margo/start/sign/Output.java
+++ src/org/margo/start/sign/Output.java
@@ -2,8 +2,7 @@ package org.margo.start.sign;
 
 public class Output {
     public static void main(String[] args) {
-
-        int y = 7;
+       /* int y = 7;
         for (int x = 1; x < 8; x++) {
             y++;
             if (x > 4) {
@@ -13,6 +12,11 @@ public class Output {
                 System.out.println(" x = " + x);
                 break;
             }
-        }
+        }*/
+        String name = new String("Hay");
+        String b = name;
+        System.out.println(new Long(9000000) == new Long(9000000));
+        System.out.println(new Long(9000000).equals(new Long(9000000)));
+
     }
 }
