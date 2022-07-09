f1fea36a7e46e8409ab3c00ba3d451bc483ca607
MargoSamo <margo_ii@mail.ru>
2022-07-09 12:26:58 +0500

    while (if-else)

--- src/org/margo/start/sign/Output.java
+++ src/org/margo/start/sign/Output.java
@@ -13,10 +13,31 @@ public class Output {
                 break;
             }
         }*/
-        String name = new String("Hay");
+
+       /* String name = new String("Hay");
         String b = name;
         System.out.println(new Long(9000000) == new Long(9000000));
         System.out.println(new Long(9000000).equals(new Long(9000000)));
-
+*/
+        int  a = 0;
+        while (a < 10) {
+            int m = 0;
+            if (a == 0 || a == 9) {
+                while (m < 20) {
+                    System.out.print("Б");
+                    m++;
+                }
+            } else {
+                while (m < 20) {
+                    if (m == 0 || m == 19)
+                        System.out.print("Б");
+                        else
+                            System.out.print(" ");
+                        m++;
+                        }
+                    }
+        System.out.println(" ");
+        a++;
+       }
     }
 }
