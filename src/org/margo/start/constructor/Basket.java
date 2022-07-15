625abb1911eb659d38974a8ef6952391d81fb5c1
MargoSamo <margo_ii@mail.ru>
2022-07-15 18:57:58 +0500

    with an error

--- src/org/margo/start/constructor/Basket.java
+++ src/org/margo/start/constructor/Basket.java
@@ -3,8 +3,11 @@ package org.margo.start.constructor;
 public class Basket {
    private final short size;
    private int price;
+
+   private Flower[] flowers;
    public Basket(short size) {
       this.size = size;
+      this.flowers = new Flower[size];
    }
    public short getSize() {
       return size;
@@ -15,9 +18,14 @@ public class Basket {
    public int getPrice() {
       return price;
    }
+   // Корзина принимает цветок
+   public void putFlower(Flower flower) {
+      flower[0] = "1";
+      }
+
    @Override
    public String toString() {
       return "Basket size: " + size + " price: " + price;
-
    }
+
 }
