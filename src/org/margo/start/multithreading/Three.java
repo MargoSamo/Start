a06a285125692e8cead9567157e3b6bd162628f3
MargoSamo <margo_ii@mail.ru>
2022-12-01 22:49:04 +0500

    synchronized

---
+++ src/org/margo/start/multithreading/Three.java
@@ -0,0 +1,41 @@
+package org.margo.start.multithreading;
+
+public class Three {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        Three three = new Three();
+
+        three.doWork();
+    }
+
+    public synchronized void increment() {
+        counter++;
+    }
+
+    public void doWork() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                    increment();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                    increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter);
+    }
+}
