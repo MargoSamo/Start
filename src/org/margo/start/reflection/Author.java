5fbe312b9bc195d002ff223418879c49abe257d5
MargoSamo <margo_ii@mail.ru>
2022-12-16 11:37:46 +0400

    Reflection

---
+++ src/org/margo/start/reflection/Author.java
@@ -0,0 +1,14 @@
+package org.margo.start.reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Author {
+    int dateOfCreation();
+
+    String name();
+}
